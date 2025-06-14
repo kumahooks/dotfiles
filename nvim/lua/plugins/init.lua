a6079ba35c22e3ec2ad09f8dccc442bab7f105d5
Bear <69819027+kumahooks@users.noreply.github.com>
2025-06-14 15:42:16 -0300

    Update init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,4 +12,41 @@ return {
 			require "configs.lspconfig"
 		end,
 	},
+
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+			ensure_installed = {
+				-- LSPs
+				"typescript-language-server",
+				"asm-lsp",
+				"bash-language-server",
+				"clangd",
+				"css-lsp",
+				"gopls",
+				"html-lsp",
+				"json-lsp",
+				"lua-language-server",
+				"marksman",
+				"pyright",
+				"rust-analyzer",
+				"yaml-language-server",
+
+				-- Formatters
+				"black",
+				"clang-format",
+				"prettier",
+				"prettierd",
+				"stylua",
+			},
+		},
+	},
+
 }
