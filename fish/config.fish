a520aeb5473a03e3cf30254a21ff1fa8dc977d15
Bear <69819027+kumahooks@users.noreply.github.com>
2025-06-14 14:33:35 -0300

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias soulseek="LD_PRELOAD=/usr/lib/libfontconfig.so.1 /usr/bin/soulseek"
+
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
