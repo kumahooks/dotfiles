6d0ca808cf7e4650768dfc1c26c182de21bb0d6f
Kuma <kuma@insiberia.net>
2025-09-11 00:14:56 -0300

    adds git blame on current line

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -59,7 +59,17 @@ return {
 	-- Buffer signs for git
 	{
 		"lewis6991/gitsigns.nvim",
-		opts = {},
+		opts = {
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+		},
 	},
 
 	-- Complete
