d73d0b7403de00cc6f0aa029d5b6fd8bd28c21d0
kumahooks <meryl@inventati.org>
2026-07-10 20:52:52 -0300

    add clear alias, local bin and opencode paths, trim fish_variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias soulseek="LD_PRELOAD=/usr/lib/libfontconfig.so.1 /usr/bin/soulseek"
+alias clear='clear && printf "\e[3J"'
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 set -gx PATH $PATH ~/go/bin
+
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+
+# opencode
+fish_add_path /home/kuma/.opencode/bin
